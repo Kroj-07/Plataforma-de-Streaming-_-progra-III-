581c218a27c879b0f4df141145f3769bf8708d86
Thiago <thiago.frias@utec.edu.pe>
2026-05-09 00:09:27 -0500

    arreglo cmake

--- src/core/Trie.h
+++ src/core/Trie.h
@@ -10,24 +10,16 @@ class Trie {
 public:
     Trie();
     ~Trie();
-    
-    // Insertar una palabra (título o sinopsis) asociada a un ID de película
-    // Inserta TODOS los sufijos para permitir búsqueda por subcadena
+
     void insertarPalabra(string palabra, int idPelicula);
-    
-    // Insertar todas las palabras de un texto (título o sinopsis completa)
+
     void insertarTexto(string texto, int idPelicula);
-    
-    // Buscar una subcadena en el Trie
-    // Retorna set con los IDs de películas que contienen esa subcadena
+
     set<int> buscarSubcadena(string subcadena);
-    
-    // Buscar una frase completa (varias palabras)
-    // Retorna la UNION de resultados de cada palabra
+
     set<int> buscarFrase(string frase);
     
 private:
-    // Nodo raíz del Trie (implementación interna)
     struct NodoTrie;
     NodoTrie* raiz;
 };
